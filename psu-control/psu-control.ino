34004e133aea7a47b3ab51285ecaea3e4318052f
Ben Morse <bjmorse@gmail.com>
2023-01-25 22:34:38 -0800

    Actual first version

--- psu-control/psu-control.ino
+++ psu-control/psu-control.ino
@@ -10,28 +10,62 @@
   model, check the Technical Specs of your board at:
   https://www.arduino.cc/en/Main/Products
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
+
+// 13 output to relay
+// 12 is input from fan
+// 2 is a copy fo the 5v (wired to 5v from the 2nd usb (not the one onboard the arduino))
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+  
+  digitalWrite(13, LOW);
+
+  delay(1000);
+  // Serial.begin(9600);
 }
 
+
 // the loop function runs over and over again forever
+// void loop() {
+//   digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+//   delay(3000);                      // wait for a second
+//   digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+//   delay(250);                      // wait for a second
+// }
+
+// 0 was off
+// 1 was on
+int state = 0;
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  while(1) {
+    if(state == 0) {
+      int fan = analogRead(A0);
+      if(fan > 15) {
+        digitalWrite(13, HIGH);
+        state = 1;
+        break;
+      }
+    }
+    if(state == 1) {
+      int fan = analogRead(A0);
+      int vin = digitalRead(2);
+      if( (fan <= 15) || (vin == 0) ) {
+        digitalWrite(13, LOW);
+        state = 0;
+        delay(1000);
+        break;
+      }
+    }
+  }
 }
+
+// void loop() {
+//   int fan = analogRead(A0);
+//   Serial.println(fan);
+//   delay(75);
+// }
